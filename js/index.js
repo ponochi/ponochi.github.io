aad519b348e937b6ee703a55d044c2b086645137
Yoshihiro Nakahara <develop@honono-bonono.jp>
2023-12-25 16:53:21 +0900

    フッターを共通化('/js/index.js')

---
+++ js/index.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+  // footer 
+  var footer_content = 'Copyright &copy; since 2023.12.24- Ponochi. All Rights Reserved.';
+  var footer = document.getElementById('footer');
+  footer.innerHTML = footer_content;
+}
